4c12e6e08f815118c97044609bc4d917dea3fc2d
Nascity@Oracle <victory8500@naver.com>
2024-07-14 04:57:20 +0000

    Working

--- flask-server/server.py
+++ flask-server/server.py
@@ -2,19 +2,19 @@ from flask import Flask # Flask
 import pymysql
 
 app = Flask(__name__)
-db = pymysql.connect(host='158.179.172.212', user='root', password='1234', db='website', charset='utf8')
+db = pymysql.connect(host='127.0.0.1', user='root', password='shajuly3', db='website', charset='utf8')
+
+
+@app.route('/')
+def default():
+    return "<h1 style='color:blue'>Hello, world!</h1>"
 
 '''
-@app.route('/users')
-def users():
-    return {"members": [{ "id" : 1, "name" : "yerin" },
-    					{ "id" : 2, "name" : "dalkong" }]}
-'''
-           
 @app.route('/board/<int:boardid>', methods=['GET'])
 def board():
     cursor = db.cursor()
     print(cursor.execute(f"SELECT * FROM posts WHERE board='{boardid}'"))
+'''
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(host='0.0.0.0')
